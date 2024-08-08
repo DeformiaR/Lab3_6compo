c59006ef7dad97b0e81c00072f770d401351c2c1
DeformiaR <113496368+DeformiaR@users.noreply.github.com>
2024-08-09 00:24:53 +0700

    Prepare Workspace

---
+++ 331-intro-to-vite/src/router/index.ts
@@ -0,0 +1,82 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '@/views/EventListView.vue'
+import AboutView from '@/views/AboutView.vue'
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
+import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
+import StudentView from '@/views/StudentView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list-view',
+      component: EventListView,
+      props: (route) => ({ page: parseInt(route.query.page?.toString() || '1') })
+    },
+    {
+      path: '/event/:id',
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: ' ',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
+    },
+
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import('../views/AboutView.vue')
+      component: AboutView
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    },
+    {
+      path: '/student',
+      name: 'student',
+      component: StudentView
+    }
+  ]
+})
+
+export default router
