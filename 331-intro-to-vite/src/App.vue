a174ce05cdd9bbe256eab7c38ce31f67d790202e
DeformiaR <113496368+DeformiaR@users.noreply.github.com>
2024-08-09 00:38:30 +0700

    update app.uve

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from './stores/message'
+import { storeToRefs } from 'pinia'
+
+const store = useMessageStore()
+const { message } = storeToRefs(store)
+
 </script>
 
 <template>
   <div id="layout">
     <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Event</RouterLink> | <RouterLink to="/about">About</RouterLink> |
-          <RouterLink to="/student">Student</RouterLink>
+          <RouterLink :to="{ name: 'passenger-list-view' }">Passenger</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
     </header>
@@ -42,5 +51,18 @@ nav a.router-link-exact-active {
 
 h2 {
   font-size: 20px;
+}
+
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
+}
+
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
 }
 </style>
